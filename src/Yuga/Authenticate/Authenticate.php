8aa0f0ee5d30db2b9f70179b74b93200d184389f
Hamid Semix <semix.hamidouh@gmail.com>
2019-12-05 11:27:28 +0300

    Correct automatic logging out of users

--- src/Yuga/Authenticate/Authenticate.php
+++ src/Yuga/Authenticate/Authenticate.php
@@ -88,7 +88,7 @@ class Authenticate extends BaseController implements IMiddleware
     public function run(Request $request, Closure $next)
     {
         if ($this->guest()) {
-            return (\Auth::authRoutesExist()) ? $this->response->redirect->route('login') : $this->response->redirect->route('yuga.auth.signin');
+            return (\Auth::authRoutesExist()) ? $this->response->redirect->route('login') : $this->response->redirect->route(env('DEFAULT_LOGIN_REDIRECT', 'login'));
             die();
         }
 
