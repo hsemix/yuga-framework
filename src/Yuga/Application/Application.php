005b2d5f3fff5ed8254dd5cd7739246226953bbb
Hamid Semix <semix.hamidouh@gmail.com>
2022-10-21 21:51:21 +0300

    Quick fix :bug:

--- src/Yuga/Application/Application.php
+++ src/Yuga/Application/Application.php
@@ -31,7 +31,7 @@ use Yuga\Interfaces\Application\Application as IApplication;
 
 class Application extends Container implements IApplication
 {
-    const VERSION = '4.2.3';
+    const VERSION = '4.2.4';
     const CHARSET_UTF8 = 'UTF-8';
 
      /**
